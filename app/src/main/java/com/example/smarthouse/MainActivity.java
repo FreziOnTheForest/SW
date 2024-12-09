1bfae489d734bc14f66615a5c454ae8a26d4a4a8
FreziOnTheForest <yana.shabalina.81@mail.ru>
2024-12-09 11:43:44 +0600

    Версия 2.0

--- app/src/main/java/com/example/smarthouse/MainActivity.java
+++ app/src/main/java/com/example/smarthouse/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.smarthouse;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Задержка в 5 секунд
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            // Запуск SignUpActivity
+            startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            // Завершение SplashScreenActivity
+            finish();
+        }, 5000);
+
     }
 }
---
+++ app/src/main/java/com/example/smarthouse/SignUpActivity.java
@@ -0,0 +1,14 @@
+package com.example.smarthouse;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
